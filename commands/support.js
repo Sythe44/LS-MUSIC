8b62b7886e67f628f10a225a0a1657c9f114b21f
Sythe <danthegamer125@gmail.com>
2024-07-30 23:17:39 +0100

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -31,7 +31,7 @@ module.exports = {
 
       const supportServerLink = "https://discord.gg/TPBgGX8HPf";
       const githubLink = "https://github.com/Sythe44";
-      const gunslink = "https://guns.lol/sythe44";
+      const gunsLink = "https://guns.lol/sythe44";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
@@ -39,7 +39,7 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join the Discord server for support and our RL and Val Services:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- guns - ${gunslinkLink}`)
+            .setDescription(`➡️ **Join the Discord server for support and our RL and Val Services:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- guns - ${gunsLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1263941942035742742/1263942902816571403/standard_1.gif?ex=66aa928e&is=66a9410e&hm=dc382bb00d831eb806ba2d71af96959ac975504bd68d3cebf2d416d4ce103d52&')
             .setTimestamp();
 
