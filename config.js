80d749146bf1c9c201daaca8eacdee9dd9eba96b
Sythe <danthegamer125@gmail.com>
2024-07-30 20:38:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,16 +23,15 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["705078573752647762", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  activityName: "➤ Created by 𝒮𝓎𝖙𝒽𝖊", // This is bot status Write Anything here 
+  SupportServer: "https://discord.gg/TPBgGX8HPf",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
-  MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
+  MusicIcon:"https://cdn.discordapp.com/attachments/1263941942035742742/1263942902816571403/standard_1.gif?ex=66a9e9ce&is=66a8984e&hm=1d8e58a8b159961327db2bce4e8f0aadf9f90a23216a5c657a1f7a25724baf0a&",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
 
