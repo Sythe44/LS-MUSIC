e8c58dcaeafd825a0fd5c16824a610b23227c2f0
Sythe <danthegamer125@gmail.com>
2025-04-25 18:24:23 +0100

    Add files via upload

---
+++ commands/support.js
@@ -0,0 +1,46 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../config.js");
+const musicIcons = require('../UI/icons/musicicons.js');
+
+module.exports = {
+    name: "support",
+    description: "Get support server link",
+    permissions: "0x0000000000000800",
+    options: [],
+    run: async (client, interaction, lang) => {
+        try {
+            const supportServerLink = "https://discord.gg/nWqjcH2fUu";
+
+            const embed = new EmbedBuilder()
+                .setColor('#b300ff')
+                .setAuthor({
+                    name: lang.support.embed.authorName,
+                    iconURL: musicIcons.beats2Icon, 
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.description
+                    .replace("{supportServerLink}", supportServerLink)
+                    .replace("{githubLink}", githubLink)
+                    .replace("{replitLink}", replitLink)
+                    .replace("{youtubeLink}", youtubeLink)
+                )
+                .setImage('https://cdn.discordapp.com/attachments/1263941942035742742/1286906797273256028/gglsSERVICES_2.gif?ex=680d099a&is=680bb81a&hm=47ae1f9198a713980864aa0709f3d45e2aa14a73f1d776926ee7d59dbccfcd4f&')
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (e) {
+            console.error(e);
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#ff0000')
+                .setAuthor({
+                    name: lang.support.embed.error,
+                    iconURL: musicIcons.alertIcon,
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.errorDescription)
+                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon });
+
+            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+    },
+};
---
+++ config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "en",
+  ownerID: ["705078573752647762", ""], 
+  mongodbUri : "mongodb+srv://Sythe44:ShivaConnect@discordbot.l4pcc6t.mongodb.net/?retryWrites=true&w=majority&appName=DiscordBot",
+  spotifyClientId : "",
+  spotifyClientSecret : "",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "Spotify", 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/nWqjcH2fUu",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
+      secure: false
+    }
+  ]
+}
